49c0d9c245c968166974023621e9f2347df36107
leonardodefendi <leo_defendi@hotmail.com>
2023-06-06 20:20:41 -0300

    Finaliza req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const especies = data.species;
+  const bicho = especies.find((especie) => especie.name === animal);
+  return bicho.residents.every((idade) => idade.age >= age);
 };
-
 module.exports = getAnimalsOlderThan;
