ed635422e3f9d30247049831145447c269184319
leonardodefendi <leo_defendi@hotmail.com>
2023-06-06 20:08:18 -0300

    Finaliza req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (id1, id2) => {
+  const array = [];
+  const especies = data.species;
+  if (id1 && id2) {
+    const animals1 = especies.find((animal) => animal.id === id1);
+    const animals2 = especies.find((animal) => animal.id === id2);
+    array.push(animals1);
+    array.push(animals2);
+  } else if (id1) {
+    const animals1 = especies.find((animal) => animal.id === id1);
+    array.push(animals1);
+  }
+  return array;
 };
 
 module.exports = getSpeciesByIds;
