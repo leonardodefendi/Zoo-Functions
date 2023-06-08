acd33846b74b6135a62d7bb90abf93e45e2012ce
leonardodefendi <leo_defendi@hotmail.com>
2023-06-08 20:11:14 -0300

    Finaliza req 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,20 +1,33 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // if (scheduleTarget) {
-  //   return data.species.find((specie) => scheduleTarget === specie.name).availability;
-  // }
-  // const obj = {};
-  // Object.keys(data.hours).forEach((day) => {
-  //   const teste = [];
-  //   data.species.forEach((specie) => ((specie.availability.includes(day)) ? teste.push(specie.name) : false));
-  //   obj[day] = {
-  //     officeHour: (teste.length === 0) ? 'CLOSED' : `Open from ${data.hours[day]
-  //       .open}am until ${data.hours[day].close}pm`,
-  //     exhibition: (teste.length === 0) ? 'The zoo will be closed!' : teste,
-  //   };
-  // });
-  // return obj;
+const genereteObj = () => {
+  const obj = {};
+  Object.keys(data.hours).forEach((day) => {
+    const teste = [];
+    data.species
+      .forEach((specie) => ((specie.availability.includes(day)) ? teste.push(specie.name) : false));
+    obj[day] = {
+      officeHour: (teste.length === 0) ? 'CLOSED' : `Open from ${data.hours[day]
+        .open}am until ${data.hours[day].close}pm`,
+      exhibition: (teste.length === 0) ? 'The zoo will be closed!' : teste,
+    };
+  });
+  return obj;
 };
-// console.log(getSchedule());
+
+const getSchedule = (scheduleTarget) => {
+  const obj = Object.keys(genereteObj());
+  const generate = genereteObj();
+  if (data.species.some((specie) => specie.name === scheduleTarget)) {
+    return data.species.find((specie) => scheduleTarget === specie.name).availability;
+  }
+  if (obj.some((day) => day === scheduleTarget)) {
+    return {
+      [scheduleTarget]: generate[scheduleTarget],
+    };
+  }
+  return generate;
+};
+console.log(getSchedule('Monday'));
+// console.log(genereteObj());
 module.exports = getSchedule;
