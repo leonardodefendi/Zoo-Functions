c9b2b4f97c63169089a6aafdd0ac2151a73b5219
leonardodefendi <leo_defendi@hotmail.com>
2023-06-08 11:58:50 -0300

    refatorando req2

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
-
+const countEntrants = (entrants) =>
+  ({
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  });
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
 };
 
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  const especies = data.species;
-  const bicho = especies.find((especie) => especie.name === animal);
-  return bicho.residents.every((idade) => idade.age >= age);
-};
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((especie) => especie.name === animal)
+  .residents.every((idade) => idade.age >= age);
 module.exports = getAnimalsOlderThan;
