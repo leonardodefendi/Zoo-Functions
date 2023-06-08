8332fc36d4b3985ba6e60a4c16747d78bf8c3af9
leonardodefendi <leo_defendi@hotmail.com>
2023-06-08 09:29:49 -0300

    Finaliza req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const aniamlList = data.species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+  if (!animal) {
+    return aniamlList;
+  } if (animal.species && animal.sex) {
+    return data.species.find((specie) => specie
+      .name === animal.species)
+      .residents.filter((animalI) => animalI.sex === animal.sex).length;
+  }
+  return aniamlList[animal.species];
 };
-
 module.exports = countAnimals;
