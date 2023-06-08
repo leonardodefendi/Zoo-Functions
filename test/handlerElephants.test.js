7071b8f5c3b6553841d4ccbf814b55ae061c3b17
leonardodefendi <leo_defendi@hotmail.com>
2023-06-08 10:10:08 -0300

    Finaliza req6 e req7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Verifica se ao passar não passar parametro retorna undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  test('Verifica se ao passar um número como parametro retorna a mensagem "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  test('Verifica se ao passamarmos como parametro "count", o retorno será a quantidade de elefeantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('Verifica se ao passamarmos como parametro "names", será um array com o nome de todos elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  test('Verifica se ao passamarmos como parametro "averageAge", o retorno será  média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  test('Verifica se ao passamarmos como parametro "location", o retorno será a localização dos elefantes', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  test('Verifica se passarmos como parametro a string "teste" o retorno será null', () => {
+    expect(handlerElephants('teste')).toBeNull();
+  });
+});
