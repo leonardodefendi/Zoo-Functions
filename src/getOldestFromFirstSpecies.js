bf51c8cc845a518c865413345bdcccf2e90bc7d8
leonardodefendi <leo_defendi@hotmail.com>
2023-06-08 15:08:11 -0300

    Finaliza req10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const findSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const findAnimal = data.species.find((specie) => specie.id === findSpecie)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.keys(findAnimal).map((caract) => findAnimal[caract]);
 };
-
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  // if (scheduleTarget) {
+  //   return data.species.find((specie) => scheduleTarget === specie.name).availability;
+  // }
+  // const obj = {};
+  // Object.keys(data.hours).forEach((day) => {
+  //   const teste = [];
+  //   data.species.forEach((specie) => ((specie.availability.includes(day)) ? teste.push(specie.name) : false));
+  //   obj[day] = {
+  //     officeHour: (teste.length === 0) ? 'CLOSED' : `Open from ${data.hours[day]
+  //       .open}am until ${data.hours[day].close}pm`,
+  //     exhibition: (teste.length === 0) ? 'The zoo will be closed!' : teste,
+  //   };
+  // });
+  // return obj;
 };
-
+// console.log(getSchedule());
 module.exports = getSchedule;
