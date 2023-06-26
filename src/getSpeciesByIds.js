2d8f4ff5f42968c2de764fe6523209845db39084
leonardodefendi <leo_defendi@hotmail.com>
2023-06-26 09:28:35 -0300

    Refatora req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  const animals = [];
-  ids.forEach((id) => animals.push(data.species.find((specie) => id === specie.id)));
-  return animals;
-};
-console.log(getSpeciesByIds());
+const getSpeciesByIds = (...ids) => ids.map((id) => data.species
+  .filter((specie) => specie.id === id)).flat();
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
