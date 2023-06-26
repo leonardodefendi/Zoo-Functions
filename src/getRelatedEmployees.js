96991dd4df58e3f087c3328be8145d28d890ff6c
leonardodefendi <leo_defendi@hotmail.com>
2023-06-26 13:30:02 -0300

    Refatora req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,13 +8,10 @@ const isManager = (id) => {
 const getRelatedEmployees = (managerId) => {
   const { employees } = data;
   if (isManager(managerId)) {
-    const array = [];
-    employees.map((employe) => employe
-      .managers.filter((id) => (id === managerId ? array.push(employe) : false)));
-    return array.map((person) => `${person.firstName} ${person.lastName}`);
+    return employees.filter(({ managers }) => managers.includes(managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
